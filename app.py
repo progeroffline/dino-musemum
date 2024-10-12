22c8c0be897bbe7dfc4e7b80afd0dfee30fa247a
progeroffline <progeroffline@gmail.com>
2024-10-13 02:53:54 +0300

    Handle notifications window and close it. Get available dinos to buy, and buy it.

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
 
-import pytesseract
 from PIL import Image
+from rich import print
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from seleniumbase import Driver
+
+from ui_detection import UiDetection
 
 
 @dataclass
@@ -25,14 +28,21 @@ class DinomuseumAutomation:
     def __init__(self, url: str):
         self.canvas_filename = "canvas.png"
         self.driver = Driver(uc=True)
+        self.actions = ActionBuilder(self.driver)
         self.driver.uc_open_with_reconnect(url, 2)
+
         self.canvas_element = self.driver.find_element("canvas")
         if self.canvas_element is None:
             print("Canvas not found!")
             self.driver.quit()
 
         self.canvas = self.get_canvas_object()
-        self.driver.save_screenshot("fullscreen.png")
+        self.ui_detector = UiDetection(
+            self.canvas_filename,
+            "./assets/buy_button.png",
+            "./assets/archeology.png",
+            "./assets/close_button.png",
+        )
 
     def get_canvas_object(self) -> Canvas:
         pos_x, pos_y = self.get_canvas_position()
@@ -45,6 +55,9 @@ class DinomuseumAutomation:
     def get_canvas_size(self) -> tuple[int, int]:
         return self.canvas_element.size["width"], self.canvas_element.size["height"]
 
+    def make_screenshot(self) -> None:
+        self.driver.save_screenshot(self.canvas_filename)
+
     def make_screenshot_by_axis(self, x1: int, y1: int, x2: int, y2: int) -> None:
         self.driver.save_screenshot(self.canvas_filename)
 
@@ -52,29 +65,45 @@ class DinomuseumAutomation:
         im = im.crop((x1, y1, x2, y2))
         im.save(self.canvas_filename)
 
-    def get_text_from_last_screenshot(self) -> str:
-        image = Image.open(self.canvas_filename)
-        text = pytesseract.image_to_string(image)
-        return text.strip().replace("  ", " ")
+    def get_center_position(
+        self, x1: int, y1: int, x2: int, y2: int
+    ) -> tuple[int, int]:
+        x = int((x1 + x2) / 2)
+        y = int((y1 + y2) / 2)
+        return x, y
 
-    def get_bottom_menu_text(self) -> str:
-        self.make_screenshot_by_axis(*self.canvas.get_bottom_menu_axis())
-        return self.get_text_from_last_screenshot()
+    def click_by_cords(self, x1: int, y1: int, x2: int, y2: int) -> None:
+        x, y = self.get_center_position(x1, y1, x2, y2)
 
-    def loading_completed(self) -> bool:
-        self.make_screenshot_by_axis(*self.canvas.get_bottom_menu_axis())
-        text = self.get_bottom_menu_text()
-        print(text)
-        return text == "Managers Upgrades @EQI Milestones Investors"
+        self.actions.pointer_action.move_to_location(x, y).click()
+        self.actions.perform()
+        self.driver.sleep(2)
+        self.make_screenshot()
 
     def wait_until_loading_not_finished(self) -> None:
-        while not self.loading_completed():
-            print("Loading still processing...")
+        self.make_screenshot()
+        while not self.ui_detector.page_loaded():
             self.driver.sleep(1)
+            self.make_screenshot()
+
+    def close_notification(self) -> None:
+        close_buttons = self.ui_detector.close_button()
+        if len(close_buttons) > 0:
+            for cords in close_buttons:
+                self.click_by_cords(*cords)
+
+    def buy_available_upgrades(self) -> None:
+        buy_buttons_cords = self.ui_detector.get_buy_buttons()
+        for cords in buy_buttons_cords:
+            self.click_by_cords(*cords)
 
     def run(self):
         self.wait_until_loading_not_finished()
-        print("Loading finished!!!")
+        self.close_notification()
+        print("Game loading finished!!!")
+
+        self.buy_available_upgrades()
+        print("All upgrades buyed!")
 
         self.driver.sleep(100000)
 
